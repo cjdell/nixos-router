60c26539515e3e683e6ee2be60c021767071c350
Chris Dell <cjdell@gmail.com>
2025-11-24 01:03:51 +0000

    Use latest kernel

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # Set your time zone.
   time.timeZone = "Europe/London";
@@ -101,5 +101,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
-
 }
